1e74f64a351d4ae656648aedf0994ac25c5af6ad
Diyorich <netherdraker2001@gmail.com>
2021-08-25 18:31:57 +0500

    Practice files of epam course

---
+++ script.js
@@ -0,0 +1,16 @@
+// // String + Number
+// let a = '2';
+// let b = 1;
+// alert(a - b + ' deducted result'); // On deducting a string of number they are deducting
+// alert(a + b + ' concatenated result'); // On summing a string and number they are concatenating
+
+// //String + boolean (False string)
+// let ab = true;
+// let bb = '';
+// alert(+ab + +bb);
+
+
+//Number + boolean
+let ac = true;
+let bc = 0;
+alert(ac + bc);
