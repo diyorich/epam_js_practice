344c0b70f7e321b21798cbd8ebd3eafbd2ab3cbb
Diyorich PC <netherdraker2001@gmail.com>
2021-09-05 23:00:46 +0500

    some changes made from home

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // String+boolean
+var empty = ''
+var not_empty = 'not empty'
+var bool = true
+var minus = empty + bool
+alert(empty + bool) // result is "true"
+alert(not_empty + bool) // result is "notemptytrue"
+
+
 
 //String+number
+var empty_string = ''
+var full_string = 'str'
+var number = 1
+alert(empty_string + number) //result is "1"
+alert(full_string + number) //result is "str1"
 
 //Number+boolean
+var numberchik = 2
+var 
 
 // String*boolean
 
