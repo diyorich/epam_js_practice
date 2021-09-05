25187bd6ca09c0583aafc91a4747f623ae04f963
Diyorich PC <netherdraker2001@gmail.com>
2021-09-05 23:34:05 +0500

    bool + string

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
-// String+boolean
-var empty = ''
-var not_empty = 'not empty'
-var bool = true
-var minus = empty + bool
-alert(empty + bool) // result is "true"
-alert(not_empty + bool) // result is "notemptytrue"
+// // String+boolean
+// var empty = ''
+// var not_empty = 'not empty'
+// var bool = true
+// var minus = empty + bool
+// alert(empty + bool) // result is "true"
+// alert(not_empty + bool) // result is "notemptytrue"
 
 
 
-//String+number
-var empty_string = ''
-var full_string = 'str'
-var number = 1
-alert(empty_string + number) //result is "1"
-alert(full_string + number) //result is "str1"
+// //String+number
+// var empty_string = ''
+// var full_string = 'str'
+// var number = 1
+// alert(empty_string + number) //result is "1"
+// alert(full_string + number) //result is "str1"
 
-//Number+boolean
-var numberchik = 2
-var 
+// //Number+boolean
+// var numberchik = 2
+// var bool_true = true
+// var bool_false = false
+// alert(numberchik + bool_true) // result is integer 3
+// alert(numberchik + bool_false) // result is integer 2
+// // String*boolean
 
-// String*boolean
-
-//String*number
-
+//String*number // Дописать примеры выше, кейсы когда в string находится не буква а цифра
+var string_to_multiple = 'string'
+var num = 2
+alert(string_to_multiple * num) //result is NaN
 //Number*boolean
 
 //String/boolean
