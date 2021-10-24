371c9b2032fa89ff3aba9d16569a9def4a15db87
Diyorich PC <netherdraker2001@gmail.com>
2021-10-24 18:10:31 +0500

    regexp

--- script1.js
+++ script1.js
@@ -2,7 +2,7 @@ let str = "ahb acb aeb aeeb adcb axeb";
 
 let reg1 = /\ba.\S\b/g;
 
-<<<<<<< HEAD
+
 let result = str.match(reg1);
 
 alert(result);
@@ -18,8 +18,8 @@ let reg3 = /\b.\Sb\b/g;
 let result_three = str.match(reg3);
 
 alert(result_three);
-=======
+
 
 
 alert(result.index)
->>>>>>> 41d3b0e3fd9dca82fd72532f3b59f2b64f7f803c
+
